2ae1b8f0c2fe5da77d0ef987cb683348b379e486
Soska <aramferin@gmail.com>
2026-02-13 10:07:49 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,22 @@
-const CACHE = 'nad-vpn-v2';
-const urls = ['/', 'manifest.json', 'icons/icon-192.png', 'icons/icon-512.png'];
+const CACHE_NAME = 'nad-vpn-v2';
+const urlsToCache = [
+  '/nad-vpn/',
+  '/nad-vpn/index.html',
+  '/nad-vpn/manifest.json',
+  '/nad-vpn/icons/icon-192.png',
+  '/nad-vpn/icons/icon-512.png'
+];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(urls))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
